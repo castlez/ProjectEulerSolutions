47d9ee1627a68126c87d746b20d9873af64cfab2
jonny Castle <castlez@live.com>
2016-02-20 12:58:19 -0800

    Starting PE 5

---
+++ ProjectEuler/SmallestMultiple/SmallestMultiple.cs
@@ -0,0 +1,20 @@
+﻿/*
+SMALLEST MULTIPLE (PROJECT EULER 5) (UNFINISHED)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestMultiple
+{
+    class SmallestMultiple
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
